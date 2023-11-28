896dd9401658fb10bc61c24e06bb595e870f4b3c
TristanBarrow <tristanmarkbarrow@gmail.com>
2023-11-28 06:58:09 -0700

    rm old code

--- old/2022/Day1_Sonar_Sweep/run.ts
+++
@@ -1,7 +0,0 @@
-// import { readFileSync } from 'fs';
-// import { calculateIncreasesAndDecreases } from "./calculateIncreasesAndDecreases";
-
-// const input = readFileSync(__dirname + '/input.txt').toString().split('\n').map(str => parseInt(str));
-
-// const output = calculateIncreasesAndDecreases(input);
-// console.log(output);
